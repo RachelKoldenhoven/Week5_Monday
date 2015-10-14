f58e6c845fc3920f4e326a7650c7f2d7c7cae57f
Rachel Koldenhoven <rachel@koldenfrozen.com>
2015-10-13 21:32:10 -0600

    homework

--- grocery.js
+++ grocery.js
@@ -1,29 +1,86 @@
 // ***You will need to add the necessary script tags for jQuery and grocery.js to index.html***
 
-$(document).ready(function(){
+$(document).ready(function() {
 
 
-
-  var groceries = [
-    {name: "Tomatoes", status: "needed", price: "3.99", quantity: 5},
-    {name: "Onions", status: "needed", price: "1.85", quantity: 2},
-    {name: "Cilantro", status: "needed", price: ".95", quantity: 1},
-    {name: "Limes", status: "complete", price: ".33", quantity: 3},
-    {name: "Jalapeno", status: "complete", price: ".15", quantity: 2}
-  ];
+    var groceries = [
+        {name: "Tomatoes", status: "needed", price: "3.99", quantity: 5},
+        {name: "Onions", status: "needed", price: "1.85", quantity: 2},
+        {name: "Cilantro", status: "needed", price: ".95", quantity: 1},
+        {name: "Limes", status: "complete", price: ".33", quantity: 3},
+        {name: "Jalapeno", status: "complete", price: ".15", quantity: 2}
+    ];
 //1. Add groceries to <ul>. Store status, price and quantity as data attributes
+
+
+    for (i = 0; i < groceries.length; i++) {
+        var currentItem = groceries[i];
+        var element = $('<li/>');
+        element.attr( "data-price", currentItem.price);
+        element.attr( "data-status", currentItem.status);
+        element.attr("data-quantity", currentItem.quantity);
+        element.text(currentItem.name);
+        $("#list").append(element);
+    }
+
 
 //2. Give 'needed' items a background-color of your choosing. Do the same for 'complete' items.
 //   Choose any text color you desire.
 
+    $('li').each(function(){
+        if($(this).attr("data-status")=== "needed"){
+            $(this).css("background-color", "red");
+        }
+
+    })
+
 //3. Display the item quantity next to it's name. (  i.e. Tomatoes (5)  )
 
+    $("li").each(function(){
+        var label = $(this).text() + " (" + $(this).attr("data-quantity") + ")";
+        $(this).text(label);
+    })
+
 //4. Display total quantity and cost on the page.
+
+    var totalQuantity = 0;
+    for (i = 0; i < groceries.length; i++) {
+        var currentItem = groceries[i];
+        totalQuantity = totalQuantity + currentItem.quantity;
+    }
+
+    $(".numItems h4").text("Total Quantity: " + totalQuantity);
+
+
+    var totalPrice = 0;
+    for(i = 0; i < groceries.length; i++) {
+        var currentItem = groceries[i];
+        totalPrice = totalPrice + currentItem.price * currentItem.quantity;
+    }
+
+    $(".totalCost h4").text("Total Cost: $" + totalPrice);
+
+
 
 //5. When an item is hovered over, it's background should darken slightly. This can be done through CSS but use
 //   JS for practice.
 
+   $("li").mouseenter(function(){
+       if ($(this).css("background-color") === ("rgb(255, 0, 0)")){
+           $(this).css("background-color", "blue");
+       } else {
+           $(this).css("background-color","black" )
+       }
+
+   })
+
+
 //6. When and item is double-clicked, give it the class 'selected' and have it's background remain slightly darkened.
+
+    //$("li").dblclick(function(){
+
+    //}
+
 
 //7. When 'Remove Item' is clicked, delete the item from the list. Have the item .fadeOut()
 
